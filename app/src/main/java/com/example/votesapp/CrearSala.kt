fd61d9952a84bf770143957ebad481bbff6cecee
root <root@localhost.localdomain>
2020-09-19 21:36:26 -0300

    Actividad crear sala

---
+++ app/src/main/java/com/example/votesapp/CrearSala.kt
@@ -0,0 +1,11 @@
+package com.example.votesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CrearSala : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_crear_sala)
+    }
+}
