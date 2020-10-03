3052f42add17b8bceb3a5302b06f57bc804a5bc3
Karen-96 <apablazakaren8@gmail.com>
2020-10-03 01:15:51 -0300

    listo Pantalla de Registro Usuario

--- app/src/main/java/com/example/votesapp/activities/registro_usuario/Registro_usuario.java
+++ app/src/main/java/com/example/votesapp/activities/registro_usuario/Registro_usuario.java
@@ -2,15 +2,33 @@ package com.example.votesapp.activities.registro_usuario;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.votesapp.R;
 
 public class Registro_usuario extends AppCompatActivity {
+    Button siguiente;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
+
+        siguiente =(Button)findViewById(R.id.botonSiguiente);
+
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent siguiente = new Intent(Registro_usuario.this,Registro_usuario2.class);
+                startActivity(siguiente);
+
+            }
+        });
+
+
     }
+
 }
---
+++ app/src/main/java/com/example/votesapp/activities/registro_usuario/Registro_usuario2.java
@@ -0,0 +1,16 @@
+package com.example.votesapp.activities.registro_usuario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.votesapp.R;
+
+public class Registro_usuario2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registro_usuario2);
+    }
+}
