0ab875f83cfd387e5cba8c8161c52380823a4f43
Fabricio <fepafumi97@gmail.com>
2020-10-04 02:18:05 -0300

    Menu inferior: Mis salas vinculado

--- app/src/main/java/com/example/votesapp/activities/mis_salas/MisSalas.kt
+++ app/src/main/java/com/example/votesapp/activities/mis_salas/MisSalas.kt
@@ -2,33 +2,58 @@ package com.example.votesapp.activities.mis_salas
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.ListAdapter
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
 import com.example.votesapp.R
 import com.example.votesapp.activities.crear_sala.CrearSala
 import com.example.votesapp.activities.lista_salas.Sala
 import java.util.*
 
-class MisSalas : AppCompatActivity() {
+class MisSalas : Fragment() {
     //Atributos
     private var slistView: ListView? = null
 
     //Llamamos el adapter
     private var sAdapter: ListAdapter? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_listado_mis_salas)
-        setSupportActionBar(findViewById(R.id.toolbar))
-        slistView = findViewById(R.id.listView)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val viewFragment = inflater.inflate(R.layout.activity_listado_mis_salas, container, false)
+        slistView = viewFragment.findViewById(R.id.listView)
 
         //!USUARIO HARDCODEADO
         val username = "Hardcodeado"
 
         //inicializamos el adaptador que va a poner la lista de sala
-        sAdapter = SalaAdapter(this,"user/$username/")
+        sAdapter = SalaAdapter(activity,"user/$username/")
+
+        //Crear adaptador y setear
+        slistView?.adapter = sAdapter
+
+        return viewFragment
+    }
+
+
+    companion object {
+        fun newInstance(): MisSalas = MisSalas()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //setContentView(R.layout.activity_listado_mis_salas)
+        //setSupportActionBar(findViewById(R.id.toolbar))
+        slistView = view?.findViewById(R.id.listView)
+
+        //!USUARIO HARDCODEADO
+        val username = "Hardcodeado"
+
+        //inicializamos el adaptador que va a poner la lista de sala
+        sAdapter = SalaAdapter(activity,"user/$username/")
 
         //Crear adaptador y setear
         slistView?.adapter = sAdapter
@@ -36,7 +61,7 @@ class MisSalas : AppCompatActivity() {
 
     fun openCrearSala(view: View){
         //Crear vinculo entre esta actividad y CrearSala
-        val intent = Intent(this, CrearSala::class.java);
+        val intent = Intent(activity, CrearSala::class.java);
         startActivity(intent)  //Ejecutar salto a la actividad
     }
 }
